6ca2b17e9b7e9f4e793d63fad42c52d16f866ff9
adeelghafoor48 <adeelghafoor48@gmail.com>
2022-02-22 11:42:55 +0500

    updated commit

--- cacheMatrix.R
+++ cacheMatrix.R
@@ -1,24 +1,40 @@
-MakeCacheMatrix <- function(x = matrix()){
-  inv <- NULL
-  set <- function(y){
+## Put comments here that give an overall description of what your
+## functions do
+##Our aim in this experiment is to write a pair of functions, namely,
+##"makeCacheMatrix" and "cacheSolve" that cache the inverse of a matrix
+##Write a short comment describing this function
+##makeCacheMatrix is a function which creates a special "matrix" object that can
+##cache its inverse for the input (which is an invertible square matrix)
+## Write a short comment describing this function
+
+setwd('C://Users//adeel/Documents/Coursera-R')
+##
+## I simply set the input x as a matrix
+## and then set the solved value "s" as a null
+## then I changed every reference to "mean" to "solve"
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  s <- NULL
+  set <- function(y) {
     x <<- y
-    inv <<- NULL
+    s <<- NULL
   }
   get <- function() x
-  setinverse <- function(inverse) {inv <<- inverse}
-  getinverse <- function() inv
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
   list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
-cacheSolve <- function(x, ...){
-  inv <- x$getinverse()
-  if(!is.null(inv)){
-    messeage("getting cached data")
-    return(inv)
-  }  
+##
+## Same here, changed "mean" to "solve" and "m" to "s"
+cacheSolve <- function(x, ...) {
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting inversed matrix")
+    return(s)
+  }
   data <- x$get()
-  inv <- solve(data, ...)
-  xset$inverse(inv)
-  inv
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
